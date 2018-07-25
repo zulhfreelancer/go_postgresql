8c0d41b5cef7c8d7f046da965b6690926ea0b64a
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-07-25 16:02:26 +0000

    Create and retrieve a book record

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
+Save and retrieve data to/from PostgreSQL database using Go.
+
 Based on tutorial here:
 
 [https://www.alexedwards.net/blog/practical-persistence-sql](https://www.alexedwards.net/blog/practical-persistence-sql)
+
+**Terminal outputs**
+
+- First run, with `createBook()`.
+
+```
+$ go run main.go
+
+******* getAllBooks() *******
+978-1503261969, Emma, Jayne Austen, £9.44
+978-1505255607, The Time Machine, H. G. Wells, £5.99
+978-1503379640, The Prince, Niccolò Machiavelli, £6.99
+
+******* getOneBook() *******
+978-1505255607, The Time Machine, H. G. Wells, £5.99
+
+******* createBook() *******
+Book new_isbn created successfully (1 row affected)
+```
+
+- Second run, without `createBook()`.
+
+```
+$ go run main.go
+
+******* getAllBooks() *******
+978-1503261969, Emma, Jayne Austen, £9.44
+978-1505255607, The Time Machine, H. G. Wells, £5.99
+978-1503379640, The Prince, Niccolò Machiavelli, £6.99
+new_isbn      , new_title, new_author, £9.99
+
+******* getOneBook() *******
+978-1505255607, The Time Machine, H. G. Wells, £5.99
+```
+
+**Database**
+
+Database name is `go_postgresql`.
+
+**SQL to create and populate table**
+
+```
+CREATE TABLE books (
+  isbn    char(14) NOT NULL,
+  title   varchar(255) NOT NULL,
+  author  varchar(255) NOT NULL,
+  price   decimal(5,2) NOT NULL
+);
+
+INSERT INTO books (isbn, title, author, price) VALUES
+('978-1503261969', 'Emma', 'Jayne Austen', 9.44),
+('978-1505255607', 'The Time Machine', 'H. G. Wells', 5.99),
+('978-1503379640', 'The Prince', 'Niccolò Machiavelli', 6.99);
+
+ALTER TABLE books ADD PRIMARY KEY (isbn);
+```
--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
   "database/sql"
   "fmt"
   "log"
+  "strconv"
 )
+
+var db *sql.DB
+
+
+
 
 type Book struct {
   isbn  string
@@ -14,11 +20,36 @@ type Book struct {
   price  float32
 }
 
-func main() {
-  db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost/go_postgresql?sslmode=disable")
+
+
+
+func init() {
+  var err error
+  dbUrl := "postgres://postgres:postgres@localhost/go_postgresql?sslmode=disable"
+  db, err = sql.Open("postgres", dbUrl)
   if err != nil {
     log.Fatal(err)
   }
+
+  if err = db.Ping(); err != nil {
+    log.Fatal(err)
+  }
+}
+
+
+
+
+func main() {
+  getAllBooks()
+  getOneBook()
+  // createBook()
+}
+
+
+
+
+func getAllBooks() {
+  fmt.Println("******* getAllBooks() *******")
 
   rows, err := db.Query("SELECT * FROM books")
   if err != nil {
@@ -42,4 +73,60 @@ func main() {
   for _, bk := range bks {
     fmt.Printf("%s, %s, %s, £%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
   }
+}
+
+
+
+
+func getOneBook() {
+  fmt.Println("******* getOneBook() *******")
+
+  isbn := "978-1505255607" // 978-1505255607
+  row := db.QueryRow("SELECT * FROM books WHERE isbn = $1", isbn)
+
+  bk := new(Book)
+  err := row.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+  if err == sql.ErrNoRows {
+    fmt.Println("Book not found")
+    return
+  } else if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Printf("%s, %s, %s, £%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+}
+
+
+
+
+func createBook() {
+  fmt.Println("******* createBook() *******")
+  isbn := "new_isbn"
+  title := "new_title"
+  author := "new_author"
+
+  if isbn == "" || title == "" || author == "" {
+    fmt.Println("One of the parameter is empty")
+    return
+  }
+  price, err := strconv.ParseFloat("9.99", 32)
+  if err != nil {
+    fmt.Println("Failed to parse string to float")
+    return
+  }
+
+  result, err := db.Exec("INSERT INTO books VALUES($1, $2, $3, $4)", isbn, title, author, price)
+  if err != nil {
+    fmt.Println("Failed to execute SQL to insert a new book")
+    return
+  }
+
+  rowsAffected, err := result.RowsAffected()
+  if err != nil {
+    fmt.Println("Failed to retrieve (x row affected) information")
+    return
+  }
+
+  fmt.Printf("Book %s created successfully (%d row affected)\n", isbn, rowsAffected)
+
 }
